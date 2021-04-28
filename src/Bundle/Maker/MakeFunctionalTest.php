d91ead33575d15fae258af156ad4bc05b7217f3c
benjaminknecht <knecht.benjamin@yahoo.de>
2021-04-28 21:11:10 +0200

    init functionality

---
+++ src/Bundle/Maker/MakeFunctionalTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\MakerBundle\ConsoleStyle;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
+use Symfony\Bundle\MakerBundle\Exception\RuntimeCommandException;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\InputConfiguration;
+use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * @author Benjamin Knecht
+ */
+final class MakeFunctionalTest extends AbstractMaker
+{
+    /** @var ManagerRegistry */
+    private $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    public static function getCommandName(): string
+    {
+        return 'make:ben-functional-test';
+    }
+
+    public function configureCommand(Command $command, InputConfiguration $inputConfig): void
+    {
+        $command
+            ->setDescription('Creates a Functional Test for a Resource')
+            ->addArgument('entity', InputArgument::OPTIONAL, 'Entity class to create a FunctionalTest for')
+        ;
+
+        $inputConfig->setArgumentAsNonInteractive('entity');
+    }
+
+    public function interact(InputInterface $input, ConsoleStyle $io, Command $command): void
+    {
+        if ($input->getArgument('entity')) {
+            return;
+        }
+
+        $argument = $command->getDefinition()->getArgument('entity');
+        $entity = $io->choice($argument->getDescription(), $this->entityChoices());
+
+        $input->setArgument('entity', $entity);
+    }
+
+    public function generate(InputInterface $input, ConsoleStyle $io, Generator $generator): void
+    {
+        $class = $input->getArgument('entity');
+
+        if (!\class_exists($class)) {
+            $class = $generator->createClassNameDetails($class, 'Entity\\')->getFullName();
+        }
+
+        if (!\class_exists($class)) {
+            throw new RuntimeCommandException(\sprintf('Entity "%s" not found.', $input->getArgument('entity')));
+        }
+
+        $entity = new \ReflectionClass($class);
+        $factory = $generator->createClassNameDetails(
+            $entity->getShortName(),
+            'Tests\\Functional',
+            'Functional'
+        );
+
+        $repository = new \ReflectionClass($this->managerRegistry->getRepository($entity->getName()));
+
+        if (0 !== \mb_strpos($repository->getName(), $generator->getRootNamespace())) {
+            // not using a custom repository
+            $repository = null;
+        }
+
+        $generator->generateClass(
+            $factory->getFullName(),
+            __DIR__.'/../Resources/skeleton/FunctionalTest.tpl.php',
+            [
+                'entity' => $entity,
+                'repository' => $repository,
+            ]
+        );
+
+        $generator->writeChanges();
+
+        $this->writeSuccessMessage($io);
+
+        $io->text([
+            'Next: Open your new FunctionalTest and finish it.',
+            'Find the documentation at https://github.com/benblub/ftg',
+        ]);
+    }
+
+    public function configureDependencies(DependencyBuilder $dependencies): void
+    {
+        // noop
+    }
+
+    private function entityChoices(): array
+    {
+        $choices = [];
+
+        foreach ($this->managerRegistry->getManagers() as $manager) {
+            foreach ($manager->getMetadataFactory()->getAllMetadata() as $metadata) {
+                $choices[] = $metadata->getName();
+            }
+        }
+
+        \sort($choices);
+
+        return $choices;
+    }
+}
---
+++ src/Bundle/Resources/skeleton/FunctionalTest.tpl.php
@@ -0,0 +1,55 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace; ?>;
+
+use <?= $entity->getName() ?>;
+use App\Test\CustomApiTestCase;
+
+final class <?= $class_name ?> extends CustomApiTestCase
+{
+    const ENDPOINT = '<?= $entityShorNameLowercase ?>';
+
+    private <?= $entityShorName ?> $entity;
+
+    public function setUp(): void
+    {
+        // @todo add your setup here
+        $this->entity = new <?= $entityShorName ?>(); // @todo create some UserObject here.. eg UserFactory::createOne as examble
+    }
+
+    public function testCreateResource()
+    {
+        $this->client->request('POST', self::ENDPOINT);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        // @todo add snapshots code here ;) ?
+    }
+
+    public function testReadResourceCollection()
+    {
+        $this->client->request('GET', self::ENDPOINT);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testUpdateResource()
+    {
+        $this->client->request('PUT', self::ENDPOINT.'/'.$entity->getId(), [
+            'json' => [
+            // @todo add prob to update here
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testDeleteResource()
+    {
+        $this->client->request('DELETE', self::ENDPOINT.'/'.$entity->getId());
+
+        $this->assertResponseStatusCodeSame(204);
+    }
+}
