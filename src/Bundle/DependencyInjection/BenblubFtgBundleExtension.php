21412deb783f2187eaece2e91e77efb10f239a7e
Benjamin Knecht <knecht.benjamin@yahoo.de>
2021-04-29 00:48:39 +0200

    add config

---
+++ src/Bundle/DependencyInjection/BenblubFtgBundleExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Benblub\Ftg\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class BenblubFtgExtension extends Extension
+{
+public function load(array $configs, ContainerBuilder $container)
+{
+    $loader = new XmlFileLoader(
+        $container,
+        new FileLocator(__DIR__.'/../Resources/config')
+    );
+    $loader->load('services.yaml');
+}
+}
