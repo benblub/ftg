a5ccd2baf6dc0d14fe34cb34a3ca11b0e2ad6a5e
benjaminknecht <knecht.benjamin@yahoo.de>
2021-05-11 11:07:54 +0200

    csfixer

--- src/Bundle/Resources/skeleton/DenyFunctionalTest.tpl.php
+++ src/Bundle/Resources/skeleton/DenyFunctionalTest.tpl.php
@@ -2,12 +2,12 @@
 
 namespace <?= $namespace; ?>;
 
-use App\Test\CustomApiTestCase;
 use App\Entity\<?= $entityShorName ?>;
+use App\Test\CustomApiTestCase;
 use App\Tests\Factory\<?= $entityShorName ?>Factory;
 use Symfony\Component\HttpFoundation\Response;
-use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\Test\Factories;
 
 
 final class <?= $class_name ?> extends CustomApiTestCase
@@ -41,7 +41,7 @@ final class <?= $class_name ?> extends CustomApiTestCase
 
     public function testDenyReadResource()
     {
-        $this->client->request('GET', self::ENDPOINT . '/' . $this->entity ->getId());
+        $this->client->request('GET', self::ENDPOINT.'/'.$this->entity->getId());
 
         $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
     }
